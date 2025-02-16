49c6ab1945d690ed4ca6f764a7a850b99269abac
RalfiSlask <nilssonnarki@gmail.com>
2025-02-16 16:20:55 +0100

    feat: add general shuffle array function and improve documentation of functions

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,13 +1,29 @@
 import { IQuestion } from '../models/IQuestion';
 
-export const shuffleQuestions = (questions: IQuestion[]): IQuestion[] => {
-  const shuffled = [...questions];
+/**
+ * Shuffles an array, used for the answers
+ * @param {T[]} array - The array to shuffle
+ * @returns {T[]} The shuffled array
+ */
+export const shuffleArray = <T>(array: T[]): T[] => {
+  const shuffled = [...array];
   for (let i = shuffled.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
   }
+  return shuffled;
+};
+
+/**
+ * Shuffles the questions and answers
+ * @param {IQuestion[]} questions - The questions to shuffle
+ * @returns {IQuestion[]} The shuffled questions
+ */
+export const shuffleQuestions = (questions: IQuestion[]): IQuestion[] => {
+  const shuffled = shuffleArray(questions);
   return shuffled.map((question, index) => ({
     ...question,
     id: index + 1,
+    answers: shuffleArray(question.answers),
   }));
 };
